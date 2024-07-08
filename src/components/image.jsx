bd0bc5f3d8d67e9d31bd5c577e22c31e5a8d1826
arixnapa <arianne.napa@gmail.com>
2024-07-08 16:46:32 +0200

    feat: add image grouping and titles to gallery component

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -3,15 +3,10 @@ import React from "react";
 export const Image = ({ title, largeImage, smallImage }) => {
   return (
     <div className="portfolio-item">
-      <div className="hover-bg">
-        {" "}
-        <a href={largeImage} title={title} data-lightbox-gallery="gallery1">
-          <div className="hover-text">
-            <h4>{title}</h4>
-          </div>
-          <img src={smallImage} className="img-responsive" alt={title} />{" "}
-        </a>{" "}
-      </div>
+      {" "}
+      <a href={largeImage} data-lightbox-gallery="gallery1">
+        <img src={smallImage} className="img-responsive" alt={title} />{" "}
+      </a>{" "}
     </div>
   );
 };
