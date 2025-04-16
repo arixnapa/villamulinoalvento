5a4f6dd524bdaee7da11349717dfccb8a69743fa
arixnapa <arianne.napa@gmail.com>
2025-04-16 15:18:49 +0200

    bold Google maps on contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -13,8 +13,8 @@ export const Contact = (props) => {
               <p>
                 <a href="https://maps.app.goo.gl/uxbGfFNpwz7G2ToQA">
                   <span>
-                    <i className="fa fa-map-marker"></i> Indirizzo su Google
-                    Maps
+                    <i className="fa fa-map-marker"></i> Indirizzo su{" "}
+                    <strong>Google Maps</strong>
                   </span>
                 </a>
                 {props.data ? props.data.address : "loading"}
